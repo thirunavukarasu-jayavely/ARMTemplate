d9d8c6d693e8eb1c3785c4fbe7556f77ff40b5b4
IN\Thirunavukarasu.Jaya <thirunavukarasu.jayavelu@fefundinfo.com>
2020-06-28 00:45:47 +0530

    First commit to PS waltk

--- ARM Templates/Azpswalkthrough.ps1
+++ ARM Templates/Azpswalkthrough.ps1
@@ -1,3 +1,13 @@
-#Getting Azure locations to see where we are able to host the resources
+#Getting all Azure locations 
 
-Get-AzLocation 
+Get-AzLocation | Format-Table Location, DisplayName -AutoSize
+
+# List resoure providers on the locations
+
+Get-AzResourceProvider -Location "southindia" | Format-Table ProviderNamespace, ResourceTypes, RegistrationState, Locations -AutoSize | Measure-Object
+
+Get-AzResourceProvider -Location "southindia" -ListAvailable | Where-Object {$_.RegistrationState -eq "NotRegistered"} | Measure-Object
+
+
+
+
